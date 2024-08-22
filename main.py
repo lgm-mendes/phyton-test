b754b2a2eb053ff86abf9c1903ce8ed872deb8a1
Lucas Mendes <lucas.cwb.ti@gmail.com>
2024-08-22 17:00:08 -0300

    Outro teste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-
 from fastapi import FastAPI
+import random
 
 app = FastAPI()
 
@@ -10,4 +9,4 @@ async def root():
 
 @app.get("/funcaoteste")
 async def funcaoteste():
-    return {"test": True, "num_aleatorio": random.randint(0, 1000)}
+    return {"test": True, "num_aleatorio": random.randint(0, 20000)}
